a8c99427e73fb4a098993569479eb64cd589d81e
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2025-05-11 22:58:51 -0400

    - Add celsius/fahrenheit modes - Add conversion to 12h time

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const apiKey = "?key=G3UKSWKT2F7MSLNWMNR7B86KA";
 
 const apiRequestBaseUrl =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-// 15-day forecast:  /[location]
-// specific dates:   /[location]/[date1]/[date2]
-//
-
-// location = address, partial address, lat/long, zip code
-// date format = yyyy-mm-dd
 
 const searchForm = document.getElementById("search");
 const searchInput = document.getElementById("city-input");
 const searchButton = document.getElementById("search-btn");
 
 let city = "Sarasota";
+let tempMode = "f";
 async function getWeather() {
   try {
     const response = await fetch(apiRequestBaseUrl + city + apiKey, {
@@ -35,21 +30,31 @@ const sunriseDisplay = document.getElementById("sunrise-time");
 const sunsetDisplay = document.getElementById("sunset-time");
 const backgroundElement = document.getElementById("today");
 
+const tempModeBtn = document.getElementById("temp-mode-btn");
+tempModeBtn.addEventListener("click", () => {
+  changeTempMode();
+  showWeather();
+});
+
 const dayDisplays = document.querySelectorAll(".day");
 console.log(dayDisplays);
 
 async function showWeather() {
   const weather = await getWeather();
   cityDisplay.innerText = weather.address;
-  currentTempDisplay.innerText = Math.round(weather.currentConditions.temp);
+  currentTempDisplay.innerText = Math.round(
+    convertTemp(weather.currentConditions.temp, tempMode)
+  );
   const backgroundJpg = weather.currentConditions.icon + ".jpg";
   console.log(weather.currentConditions.icon);
   console.log(backgroundJpg);
   backgroundElement.style.backgroundImage = `url("${backgroundJpg}")`;
-  currentHighDisplay.innerText = Math.round(weather.days[0].tempmax);
+  currentHighDisplay.innerText = Math.round(
+    convertTemp(weather.days[0].tempmax, tempMode)
+  );
   currentLowDisplay.innerText = Math.round(weather.days[0].tempmin);
-  sunriseDisplay.innerText = weather.days[0].sunrise.slice(0, 5);
-  sunsetDisplay.innerText = weather.days[0].sunset.slice(0, 5);
+  sunriseDisplay.innerText = convertTime(weather.days[0].sunrise);
+  sunsetDisplay.innerText = convertTime(weather.days[0].sunset);
 
   let dayIndex = new Date().getDay() + 1;
   for (let i = 0; i < 7; i++) {
@@ -60,10 +65,10 @@ async function showWeather() {
     const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     dayDisplays[i].querySelector(".weekday").textContent = dayNames[dayIndex];
     dayDisplays[i].querySelector(".high-temp").textContent = Math.round(
-      weather.days[i + 1].tempmax
+      convertTemp(weather.days[i + 1].tempmax, tempMode)
     );
     dayDisplays[i].querySelector(".low-temp").textContent = Math.round(
-      weather.days[i + 1].tempmin
+      convertTemp(weather.days[i + 1].tempmin, tempMode)
     );
     let svg = weather.days[i + 1].icon + ".svg";
     dayDisplays[i].querySelector("img").src = svg;
@@ -82,5 +87,29 @@ searchForm.addEventListener("submit", (e) => {
     showWeather();
   }
 });
+
+function convertTemp(value, units) {
+  if (units === "f") {
+    return value;
+  } else {
+    return ((value - 32) * 5) / 9;
+  }
+}
+
+function convertTime(time) {
+  const [h, m] = time.split(":");
+  const meridian = h >= 12 ? "PM" : "AM";
+  const hour = h % 12 === 0 ? 12 : h % 12;
+  return `${hour}:${m} ${meridian}`;
+}
+
+function changeTempMode() {
+  if (tempMode === "f") {
+    tempMode = "c";
+  } else {
+    tempMode = "f";
+  }
+  document.getElementById("temp-mode").innerText = tempMode.toUpperCase();
+}
 
 showWeather();
